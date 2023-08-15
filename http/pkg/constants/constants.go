9cdfa714feaf8063019f6d4113e8e793c5dca096
scoxty <xty1821901@163.com>
2023-08-15 22:13:39 +0800

    修改constants

--- http/pkg/constants/constants.go
+++ http/pkg/constants/constants.go
@@ -1,7 +1,9 @@
 package constants
 
 const (
-	UserTableName  string = "user"
-	VideoTableName string = "video"
-	VideoPrefix    string = "https://6773cee61173a963ce88952332a1d2f6-app.1024paas.com/home/runner/app/http/internal/video/"
+	UserTableName         string = "user"
+	VideoTableName        string = "video"
+	UserFavoriteTableName string = "user_favorite"
+	UserFollowTableName   string = "user_follow"
+	VideoPrefix           string = "https://6773cee61173a963ce88952332a1d2f6-app.1024paas.com/home/runner/app/http/internal/video/"
 )
