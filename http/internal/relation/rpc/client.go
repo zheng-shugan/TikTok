bc1669f7f2bb788a0278b56be78235e1ad3add4f
sunflower10086 <lz18738377974@163.com>
2023-08-25 15:44:39 +0800

    实现了互动接口和社交接口的client端代码

---
+++ http/internal/relation/rpc/client.go
@@ -0,0 +1,80 @@
+package rpc
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/sunflower10086/TikTok/http/config"
+	___relation "github.com/sunflower10086/TikTok/relation/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var relationClient ___relation.RelationClient
+
+func init() {
+	relationConf := config.C().Apps.Relation
+	Addr := fmt.Sprintf("%s:%s", relationConf.Host, relationConf.Port)
+	conn, err := grpc.Dial(Addr, grpc.WithBlock(), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	relationClient = ___relation.NewRelationClient(conn)
+}
+
+func RelationAction(ctx context.Context, request *___relation.ActionRequest) error {
+	_, err := relationClient.Action(ctx, request)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func FollowList(ctx context.Context, request *___relation.FollowListRequest) (*___relation.FollowListResponse, error) {
+	resp, err := relationClient.FollowList(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func FollowerList(ctx context.Context, request *___relation.FollowerListRequest) (*___relation.FollowerListResponse, error) {
+	resp, err := relationClient.FollowerList(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func FriendList(ctx context.Context, request *___relation.FriendListRequest) (*___relation.FriendListResponse, error) {
+	resp, err := relationClient.FriendList(ctx, request)
+	if err != nil {
+		return resp, err
+	}
+
+	return resp, nil
+}
+
+func MessageChat(ctx context.Context, request *___relation.MessageChatRequest) (*___relation.MessageChatResponse, error) {
+	resp, err := relationClient.MessageChat(ctx, request)
+	if err != nil {
+		return resp, err
+	}
+
+	return resp, nil
+}
+
+func MessageAction(ctx context.Context, request *___relation.MessageActionRequest) (*___relation.MessageActionResponse, error) {
+	resp, err := relationClient.MessageAction(ctx, request)
+	if err != nil {
+		return resp, err
+	}
+
+	return resp, nil
+}
--- http/main.go
+++ http/main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/sunflower10086/TikTok/http/config"
 	"github.com/sunflower10086/TikTok/http/internal/dao/db"
+	_ "github.com/sunflower10086/TikTok/http/internal/relation/rpc"
 	"github.com/sunflower10086/TikTok/http/internal/router"
 )
 
@@ -23,10 +23,6 @@ func main() {
 	r := gin.Default()
 
 	router.Init(r)
-
-	r.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, "hello")
-	})
 
 	httpConf := config.C().Apps.HTTP
 	addr := fmt.Sprintf("%s:%s", httpConf.Host, httpConf.Port)
